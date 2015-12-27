696fe563c7467d57dff469985538732bb4cded84
Matt Petrowsky <matt@isoproductions.com>
2015-12-26 20:13:21 -0800

    Adding vim-plug to vim's autoload

--- vim/install.sh
+++ vim/install.sh
@@ -2,3 +2,6 @@
 
 echo "Creating vim directories"
 mkdir -p ~/.vim-tmp
+
+echo "Installing vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
